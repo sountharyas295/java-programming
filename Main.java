4774e250071ef62b5bdcfeb13717629fa6f77567
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2018-09-07 12:35:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,sum,diff,mul,div;
+    a=10;
+    b=2;
+    sum=a+b;
+    diff=a-b;
+    mul=a*b;
+    div=a/b;
+     System.out.println("sum:"+sum);
+     System.out.println("diff:"+diff);
+     System.out.println("mul:"+mul);
+     System.out.println("div:"+div);
+    }
+  }
