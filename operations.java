367c85ce3b6dfeb6264f0a4aa93f3181eb841d6a
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2018-09-15 15:40:41 +0530

    Create operations.java

---
+++ operations.java
@@ -0,0 +1,25 @@
+class Main {
+  public static void main(String[] args) {
+    int a=5,b=4;
+    int c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s;
+    c=a+b;
+
+d=a-b;
+e=a*b;
+f=b/a;
+g=b%a;
+h=b++;
+i=b—;
+j=a&b;
+k=a|b;
+l=a^b;
+m=~a;
+n=a<<02;
+o=>>2;
+p=a>>>2;
+q=a&&b;
+r=a||b;
+s=!(a&&b);
+system.out.println(c+''d+''e+''f+''g+''h+''i+''j+''k+''l+''m+''n+''o+''p+''q+''r+''s+'');    
+  }
+}
