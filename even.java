e747f241f1a2550db9100613e89266be319026dc
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2018-10-03 12:32:00 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int num;
+      
+         num=10;
+          if(num%2==0)
+            System.out.println(num + "the num is even");
+        else 
+            System.out.println(num + "the num is odd ");
+       
+         
+  }
+}
