502e768f22af038570c84f557125023bd7badc7b
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2018-10-03 11:48:25 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    int num;
+      
+         num=10;
+        System.out.print("Enter any integer number: ");
+       
+         
+        
+        if(num>0)
+            System.out.println(num + " is POSITIVE NUMBER.");
+        else if(num<0)
+            System.out.println(num + " is NEGATIVE NUMBER.");
+        else
+            System.out.println("IT's ZERO.");
+         
+  }
+}
