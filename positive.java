e31fcbba036caa367f8679c3e11229eacfc07223
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2018-10-03 12:02:53 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
     int num;
-      num=10;
+     if( num=10)
     System.out.println(num+"it is positive");
     else
       System.out.println(num+"it is negative");
