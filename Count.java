3713a6482df349261f2ad1fd9bebab7feae55f52
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2018-10-04 16:54:21 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,8 @@
+class Count {
+  public static void main(String[] args) {
+  int num =12345;
+  count = 0;
+   for(; num != 0; num/=10, ++count)   
+   System.out.println("Number of digits: " + count);
+    }
+}
