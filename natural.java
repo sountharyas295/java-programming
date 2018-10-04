a91fbb75d97b7d0b00db4968ce42f6a09b34d75f
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2018-10-04 12:05:03 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+   int num= 1000; 
+   int sum=0 ;
+   for(int i = 1; i <= num; ++i)
+   sum = sum+i;
+    System.out.println("sum is"+ sum );
+  }
+}
