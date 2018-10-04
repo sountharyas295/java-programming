84751227313e54abdc226bb2e0b61ce549aab632
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2018-10-04 11:47:57 +0530

    Create leap year.java

---
+++ leap year.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+   int year;
+   year = 2016;
+   if(year%4==0)
+   System.out.println(year+" is leap year");
+   else
+   System.out.println(year+"is not leap year");
+  }
+}
