fe8fbf7d57e0173e4aa3901723cbd30bcd05cfd7
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2018-10-04 11:40:32 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+   char ch = 's';
+   if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            System.out.println(ch + " is an alphabet.");
+        else
+            System.out.println(ch + " is not an alphabet.");
+  }
+}
