39490ecd61e336d1e3b6168b28436be0b3361dda
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2019-02-12 12:25:38 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,10 @@
+class Even {
+  public static void main(String[] args) {
+			System.out.println(" even numbers are: " );
+      for(int i=1; i <=20; i++){
+			if( i % 2 == 0){
+				System.out.print(i + " ");
+    }
+}
+  }
+  }
