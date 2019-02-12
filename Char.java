03c05ebc26b73d43f57f0559b9deabb249d7a5c0
sountharyas295 <43053646+sountharyas295@users.noreply.github.com>
2019-02-12 12:08:58 +0530

    Create Char.java

---
+++ Char.java
@@ -0,0 +1,8 @@
+class Char {
+  public static void main(String[] args) {
+    String str = "journaldev.com";
+		char[] charArr = str.toCharArray();
+		System.out.println("String converted to char array: " 
+							+ Arrays.toString(charArr));
+  }
+}
